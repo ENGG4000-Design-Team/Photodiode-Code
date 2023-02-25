e59ed812b61cc9442412eca702cf5d9b241d3d82
EthanG78 <ethangarnier78@gmail.com>
2023-02-25 17:19:00 -0400

    comparing voltages in singleended_rpi.cpp test

--- singleended_rpi.cpp
+++ singleended_rpi.cpp
@@ -8,20 +8,38 @@ Adafruit_ADS1015 ads;
 
 int main()
 {
-    uint16_t adc0, adc1, adc2, adc3;
+    int16_t adc0, adc1, adc2, adc3;
+    float volts0, volts1, volts2, volts3;
 
     int sadc0;
 
     ads.setGain(GAIN_ONE);
-    ads.begin();
+
+    if (!ads.begin())
+    {
+        std::cout << "Failed to initialize ADS." << std::endl;
+        while (1)
+            ;
+    }
+
     while (true)
     {
+
         adc0 = ads.readADC_SingleEnded(0);
-        sadc0 = static_cast<int>(adc0);
         adc1 = ads.readADC_SingleEnded(1);
         adc2 = ads.readADC_SingleEnded(2);
         adc3 = ads.readADC_SingleEnded(3);
-        std::cout << "\nadc0 = (" << sadc0 << ", " << sadc0 << "), adc1 = " << adc1 << ", adc2 = " << adc2 << ", adc3 = " << adc3 << std::endl;
+
+        volts0 = ads.computeVolts(adc0);
+        volts1 = ads.computeVolts(adc1);
+        volts2 = ads.computeVolts(adc2);
+        volts3 = ads.computeVolts(adc3);
+
+        std::cout << "-----------------------------------------------------------" << std::endl;
+        std::cout << "AIN0: " << adc0 << "  " << volts0 << "V" << std::endl;
+        std::cout << "AIN1: " << adc1 << "  " << volts1 << "V" << std::endl;
+        std::cout << "AIN2: " << adc2 << "  " << volts2 << "V" << std::endl;
+        std::cout << "AIN3: " << adc3 << "  " << volts3 << "V" << std::endl;
 
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
