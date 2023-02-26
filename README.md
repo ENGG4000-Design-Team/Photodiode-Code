088ca75baa769650422ac840f71cf87ca3bdbabf
EthanG78 <ethangarnier78@gmail.com>
2023-02-26 13:17:08 -0400

    starting to document the mappings of GPIO ports to photodiodes on the board

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Photodiode Code
 A repository to hold all code related to reading data from the photodiode array part of the Balloon Borne Solar Telescope. This data is being read from an [Adafruit ADS1015 12-bit ADC](https://www.adafruit.com/product/1083).
+
+## Mux Channel to Photodiode Table
+Describes what mux channel corresponds to what photodiode on which analog input.
+
+### Analog Input 0
+- 0000 => Photodiode D1
+- 0001
+- 0010
+- 0011
+- 0100
+- 0101
+- 0110
+- 0111
+- 1000
+- 1001
+- 1010
+- 1011
+- 1100
+- 1101
+- 1110
+- 1111
--- singleended_rpi.cpp
+++ singleended_rpi.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <chrono>
 #include <thread>
 
@@ -8,10 +9,10 @@ Adafruit_ADS1015 ads;
 
 int main()
 {
-    int16_t adc0, adc1, adc2, adc3;
-    float volts0, volts1, volts2, volts3;
+    uint16_t adc0, adc1, adc2, adc3;
+    // float volts0, volts1, volts2, volts3;
 
-    int sadc0;
+    // int sadc0;
 
     ads.setGain(GAIN_ONE);
 
@@ -22,26 +23,29 @@ int main()
             ;
     }*/
 
-    ads.begin()
+    ads.begin();
 
     while (true)
     {
-
         adc0 = ads.readADC_SingleEnded(0);
         adc1 = ads.readADC_SingleEnded(1);
         adc2 = ads.readADC_SingleEnded(2);
         adc3 = ads.readADC_SingleEnded(3);
 
-        //volts0 = ads.computeVolts(adc0);
-        //volts1 = ads.computeVolts(adc1);
-        //volts2 = ads.computeVolts(adc2);
-        //volts3 = ads.computeVolts(adc3);
+        // volts0 = ads.computeVolts(adc0);
+        // volts1 = ads.computeVolts(adc1);
+        // volts2 = ads.computeVolts(adc2);
+        // volts3 = ads.computeVolts(adc3);
+
+        std::cout << std::showbase
+                  << std::internal
+                  << std::setfill('0');
 
         std::cout << "-----------------------------------------------------------" << std::endl;
-        std::cout << "AIN0: " << adc0 << std::endl; //"  " << volts0 << "V" << std::endl;
-        std::cout << "AIN1: " << adc1 << std::endl; //"  " << volts1 << "V" << std::endl;
-        std::cout << "AIN2: " << adc2 << std::endl; //"  " << volts2 << "V" << std::endl;
-        std::cout << "AIN3: " << adc3 << std::endl; //"  " << volts3 << "V" << std::endl;
+        std::cout << "AIN0: " << std::hex << std::setw(6) << adc0 << std::endl; //"  " << volts0 << "V" << std::endl;
+        std::cout << "AIN1: " << std::hex << std::setw(6) << adc1 << std::endl; //"  " << volts1 << "V" << std::endl;
+        //std::cout << "AIN2: " << std::hex << std::setw(6) << adc2 << std::endl; //"  " << volts2 << "V" << std::endl;
+        //std::cout << "AIN3: " << std::hex << std::setw(6) << adc3 << std::endl; //"  " << volts3 << "V" << std::endl;
 
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
